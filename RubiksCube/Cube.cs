49f90bf1eacc695fc4dbca043a1297fdbc96153f
Nicholas Montemorano <nmm9422@rit.edu>
2018-08-11 22:19:33 -0400

    Initial commit

---
+++ RubiksCube/Cube.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCube
+{
+    enum Colors { Red, Yellow, Green, Blue, Orange, White };
+
+    class Side
+    {
+        public Colors[,] Data;
+        public AdjacentVector[] AdjacentRows;
+
+        public Side(Colors c)
+        {
+            this.Data = new Colors[,] { { c, c, c }, { c, c, c }, { c, c, c } };
+        }
+
+        public Colors[] GetColumn(int index)
+        {
+            return new Colors[] { this.Data[0, index], this.Data[1, index], this.Data[2, index] };
+        }
+
+        public Colors[] GetRow(int index)
+        {
+            return new Colors[] { this.Data[index, 0], this.Data[index, 1], this.Data[index, 2] };
+        }
+
+        public void SetColumn(Colors[] colors, int index)
+        {
+            this.Data[0, index] = colors[0];
+            this.Data[1, index] = colors[1];
+            this.Data[2, index] = colors[2];
+        }
+
+        public void SetRow(Colors[] colors, int index)
+        {
+            this.Data[index, 0] = colors[0];
+            this.Data[index, 1] = colors[1];
+            this.Data[index, 2] = colors[2];
+        }
+      
+        public void Rotate()
+        {
+            RotateAdjacentVectors();
+            RotateFace();
+        }
+        
+        // Updates the adjacent rows in the Side objects "touching" the
+        // Side object that was rotated.
+        public void RotateAdjacentVectors()
+        {
+            int length = this.AdjacentRows.Length;
+            Colors[] temp = new Colors[3];
+            Array.Copy(this.AdjacentRows[length - 1].GetAdjacentVector(), temp, 3);
+
+            for (int i = length - 1; i > 0; i--)
+            {
+                AdjacentVector current = this.AdjacentRows[i];
+                AdjacentVector previous = this.AdjacentRows[i - 1];
+
+                current.SetVector(previous.GetAdjacentVector());
+            }
+
+            this.AdjacentRows[0].SetVector(temp);
+        }
+
+        // Updates the position of each piece of data in a Side object that
+        // has been rotated.
+        public void RotateFace()
+        {
+            int xLength = this.Data.GetLength(0);
+            int yLength = this.Data.GetLength(1);
+
+            Colors[,] rotated = new Colors[xLength, yLength];
+
+            for (int i = 0; i < xLength; i++)
+            {
+                for (int j = 0; j < yLength; j++)
+                {
+                    rotated[i, j] = this.Data[xLength - j - 1, i];
+                }
+            }
+
+            this.Data = rotated;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+
+            for (int x = 0; x < this.Data.GetLength(0); x += 1)
+            {
+                for (int y = 0; y < this.Data.GetLength(1); y += 1)
+                {
+                    str += this.Data[x, y] + " ";
+                }
+
+                str += "\n";
+            }
+
+            return str;
+        }
+    }
+
+    class AdjacentVector
+    {
+        public Side Side;
+        public int Index;
+        public bool IsRow;
+
+        // Generate a vector affected by the Side object that 
+        // created it.
+        public AdjacentVector(Side side, int index, bool isRow)
+        {
+            this.Side = side;
+            this.Index = index;
+            this.IsRow = isRow;
+        }
+
+        public Colors[] GetAdjacentVector()
+        {
+            if (this.IsRow)
+            {
+                return this.Side.GetRow(this.Index);
+            }
+            else
+            {
+                return this.Side.GetColumn(this.Index);
+            }
+        }
+
+        public void SetVector(Colors[] colors)
+        {
+            if (IsRow)
+            {
+                this.Side.SetRow(colors, this.Index);
+            }
+            else
+            {
+                this.Side.SetColumn(colors, this.Index);
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = ""; 
+            foreach (Colors color in this.GetAdjacentVector())
+            {
+                if (this.IsRow)
+                {
+                    str += color + " ";
+                }
+                else
+                {
+                    str += color + "\n";
+                }
+            }
+
+            return str;
+        }
+
+    }
+
+    partial class Program
+    {
+        /*
+        public static void Main()
+        {
+            Cube c = new Cube();
+
+            Cube.Action[] actions = new Cube.Action[]
+            {
+                Cube.Action.Front,
+                Cube.Action.Right,
+                Cube.Action.Left
+            };
+
+            foreach (Cube.Action action in actions) {
+                cube.Execute(action);
+                Console.WriteLine(cube);
+            }
+
+            cube.Execute(Cube.Action.Front);
+            Console.WriteLine(cube);
+            Console.Read();
+            
+        }
+        */
+    }
+}
---
+++ RubiksCube/CubeRepresentation.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCube
+{
+    public class Cube
+    {
+        public enum Action
+        {
+            Front,
+            FrontPrime,
+            Upper,
+            UpperPrime,
+            Right,
+            RightPrime,
+            Back,
+            BackPrime,
+            Left,
+            LeftPrime,
+            Down,
+            DownPrime
+        }
+
+        Side Upper;
+        Side Back;
+        Side Right;
+        Side Left;
+        Side Front;
+        Side Down;
+
+        public Cube()
+        {
+            this.Upper = new Side(Colors.White);
+            this.Back = new Side(Colors.Yellow);
+            this.Right = new Side(Colors.Red);
+            this.Left = new Side(Colors.Orange);
+            this.Front = new Side(Colors.Green);
+            this.Down = new Side(Colors.Blue);
+
+            // Wires up the proper Side object vectors touching the face of 
+            // the 'back' Side object.
+            AdjacentVector upperBackConnection = new AdjacentVector(this.Back, 0, true);
+            AdjacentVector upperFrontConnection = new AdjacentVector(this.Front, 0, true);
+            AdjacentVector upperLeftConnection = new AdjacentVector(this.Left, 0, true);
+            AdjacentVector upperRightConnection = new AdjacentVector(this.Right, 0, true);
+
+            // The initial order of this array is critical, as it represents
+            // the actual physical cube and must be consistent across Side objects.
+            this.Upper.AdjacentRows = new AdjacentVector[] { upperBackConnection, upperRightConnection, upperFrontConnection, upperLeftConnection };
+
+            AdjacentVector backUpperConnection = new AdjacentVector(this.Upper, 0, true);
+            AdjacentVector backLeftConnection = new AdjacentVector(this.Left, 0, false);
+            AdjacentVector backDownConnection = new AdjacentVector(this.Down, 2, true);
+            AdjacentVector backRightConnection = new AdjacentVector(this.Right, 2, false);
+
+            this.Back.AdjacentRows = new AdjacentVector[] { backUpperConnection, backLeftConnection, backDownConnection, backRightConnection };
+
+            AdjacentVector rightTopConnection = new AdjacentVector(this.Upper, 2, false);
+            AdjacentVector rightBackConnection = new AdjacentVector(this.Back, 0, false);
+            AdjacentVector rightDownConnection = new AdjacentVector(this.Down, 2, false);
+            AdjacentVector rightFrontConnection = new AdjacentVector(this.Front, 2, false);
+
+            this.Right.AdjacentRows = new AdjacentVector[] { rightTopConnection, rightBackConnection, rightDownConnection, rightFrontConnection };
+
+            AdjacentVector downFrontConnection = new AdjacentVector(this.Front, 2, true);
+            AdjacentVector downRightConnection = new AdjacentVector(this.Right, 2, true);
+            AdjacentVector downBackConnection = new AdjacentVector(this.Back, 2, true);
+            AdjacentVector downLeftConnection = new AdjacentVector(this.Left, 2, true);
+
+            this.Down.AdjacentRows = new AdjacentVector[] { downFrontConnection, downRightConnection, downBackConnection, downLeftConnection };
+
+            AdjacentVector frontUpperConnection = new AdjacentVector(this.Upper, 2, true);
+            AdjacentVector frontRightConnection = new AdjacentVector(this.Right, 0, false);
+            AdjacentVector frontDownConnection = new AdjacentVector(this.Down, 0, true);
+            AdjacentVector frontLeftConnection = new AdjacentVector(this.Left, 2, false);
+
+            Front.AdjacentRows = new AdjacentVector[] { frontUpperConnection, frontRightConnection, frontDownConnection, frontLeftConnection };
+
+            AdjacentVector leftUpperConnection = new AdjacentVector(this.Upper, 0, false);
+            AdjacentVector leftFrontConnection = new AdjacentVector(this.Front, 0, false);
+            AdjacentVector leftDownConnection = new AdjacentVector(this.Down, 0, false);
+            AdjacentVector leftBackConnection = new AdjacentVector(this.Back, 2, false);
+
+            this.Left.AdjacentRows = new AdjacentVector[] { leftUpperConnection, leftFrontConnection, leftDownConnection, leftBackConnection };
+        }
+
+        public void Execute(Action action)
+        {
+            Dictionary<Action, Side> sides = new Dictionary<Action, Side>
+            {
+                { Action.Front, this.Front }, { Action.FrontPrime, this.Front },
+                { Action.Upper, this.Upper }, { Action.UpperPrime, this.Upper },
+                { Action.Right, this.Right }, { Action.RightPrime, this.Right },
+                { Action.Back, this.Back }, { Action.BackPrime, this.Back },
+                { Action.Left, this.Left }, { Action.LeftPrime, this.Left },
+                { Action.Down, this.Right }, { Action.DownPrime, this.Down },
+            };
+            Side side = sides[action];
+            
+            switch (action)
+            {
+                case Action.Front:
+                case Action.Upper:
+                case Action.Right:
+                case Action.Back:
+                case Action.Left:
+                case Action.Down:
+                    this.RotateFaceClockwise(side);
+                    break;
+                case Action.FrontPrime:
+                case Action.UpperPrime:
+                case Action.RightPrime:
+                case Action.BackPrime:
+                case Action.LeftPrime:
+                case Action.DownPrime:
+                    this.RotateFaceClockwise(side);
+                    break;
+            }
+        }
+
+        private void RotateFaceClockwise(Side face)
+        {
+            face.Rotate();
+        }
+
+        private void RotateFaceCounterClockwise(Side face)
+        {
+            face.Rotate();
+            face.Rotate();
+            face.Rotate();
+        }
+
+        public override string ToString()
+        {
+            Dictionary<string, Side> sides = new Dictionary<string, Side>
+            {
+                { "FRONT", this.Front },
+                { "UPPER", this.Upper },
+                { "BACK", this.Back },
+                { "LEFT", this.Left },
+                { "RIGHT", this.Right },
+                { "DOWN", this.Down },
+            };
+
+            StringBuilder str = new StringBuilder();
+            foreach (KeyValuePair<string, Side> entry in sides)
+            {
+                str.AppendLine(entry.Key);
+                str.AppendLine(entry.Value.ToString());
+                str.AppendLine();
+            }
+
+
+            return str.ToString();
+        }
+
+        public static void Main()
+        {
+            Cube c = new Cube();
+
+            c.RotateFaceClockwise(c.Left);
+            Console.WriteLine(c.Upper);
+            c.RotateFaceClockwise(c.Left);
+            Console.WriteLine(c.Upper);
+            c.RotateFaceClockwise(c.Left);
+            Console.WriteLine(c.Upper);
+            c.RotateFaceClockwise(c.Left);
+            Console.WriteLine(c.Upper);
+
+            Console.Read();
+        }
+    }
+}
